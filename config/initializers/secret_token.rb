4f206115ef2c109a0c17e8560d9288a982119f56
werdyl <eddywerdy@gmail.com>
2013-11-12 22:55:00 -0500

    first commit, blank Rails project with GeoCoder installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Populux::Application.config.secret_token = '1d70f05846085c01aefc9fbee585da9bf3af6fb273c24e1a86303509a38a2be22ffb2b0166f67f12257ce1888070067f6a14501012d50c34d82653d0a0614d0e'
